1eadf1096edc6b17a73ac9e3c865a32068da02fb
HuyNguyen <huuhuybn@gmail.com>
2026-07-14 20:01:30 +0700

    ok

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,26 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {   bool isShowStory = false;
     public GameObject story;
-    
+    AudioSource audioSource;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void Play()
     {
+        audioSource.Play();
         Debug.Log("Play");
         // truyen vao ten cua Scene
         SceneManager.LoadScene("SampleScene");
     }
     public void Story()
     {   
+        audioSource.Play();
         // hien thi story
         Debug.Log("Story");
         if (story.activeSelf){
@@ -24,11 +32,13 @@ public class MainMenu : MonoBehaviour
     }
     public void Setting()
     {
+        audioSource.Play();
         // chuyeen sang scene Setting
         Debug.Log("Setting");
     }
     public void Exit()
     {
+        audioSource.Play();
         Debug.Log("Exit");
         Application.Quit();
     }
