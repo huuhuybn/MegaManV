9e1f751b6c196f43c05e1df0dc9f7577795e6ce1
HuyNguyen <huuhuybn@gmail.com>
2026-07-16 15:57:09 +0700

    ssss

---
+++ Assets/Scripts/HPBarController.cs
@@ -0,0 +1,37 @@
+using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBarController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Slider slider;
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+    
+    
+    private void UpdateHP(int hp)
+    {
+        // cap nhat giao dien cho hp tai day 
+        if (slider.value > 17)
+        {
+            slider.value = 17;
+        }else if (slider.value < 0)
+        {
+            slider.value = 0;
+        }
+        else
+        {
+            slider.value += hp;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
